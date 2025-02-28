ac83fb66f75bf9da0e1ee7af39fd59312ac92a7b
veressRobert <160704842+veressRobert@users.noreply.github.com>
2025-02-28 23:26:01 +0200

    loading screen done

--- app/screens/loadingScreen.tsx
+++ app/screens/loadingScreen.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Image, ActivityIndicator } from 'react-native';
 import { Text, View } from 'react-native';
 import { globalStyles } from '../styles/styles';
 
@@ -6,7 +7,16 @@ const loadingScreen = () => {
     return (
         <View
             style={globalStyles.background}>
-            <Text style = {globalStyles.text}>loading screen</Text>
+            <View style={{ flex: 1}}>
+                <Image style={{ width: '35%', resizeMode: 'contain', alignSelf:'center' }} source={require('../../assets/images/logo1.jpg')} />
+            </View>
+            <View style={{ flex: 1 }} />
+            <ActivityIndicator animating={true} 
+                color="#FC9104"
+                size="large"/>
+            <View style={{ flex: 1 }} />
+            <View style={{ flex: 1 }} />
+
         </View>
     );
 };
