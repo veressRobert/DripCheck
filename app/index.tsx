0ceafbdb753d2d28e2e6de13c53e3426054c68c5
veressRobert <160704842+veressRobert@users.noreply.github.com>
2025-03-01 11:01:34 +0200

    index to loading

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,11 @@
 import { Button, Text, View } from "react-native";
 import * as React from "react";
-import {Link} from "expo-router";
+import {Redirect} from "expo-router";
 
 function Index() {
   return (
     <View>
-      <Link href={"/screens/loadingScreen"}><Button title={"loadingScreen"} /> Go to loading</Link>
-      <Link href={"/screens/loginScreen"}><Button title={"loginScreen"} /> Go to login</Link>
-      <Link href={"/screens/forgotPasswordScreen"}><Button title={"forgotPasswordScreen"} /> Go to forgot password</Link>
-      <Link href={"/screens/registerScreen"}><Button title={"registerScreen"} /> Go to register</Link>
+      <Redirect href={"/screens/loadingScreen"} />
     </View>
   );
 }
